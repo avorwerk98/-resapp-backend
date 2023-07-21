76f6bd04ade6a9d9be09794fe6a900d084e8c838
avery <avorwerk@bu.edu>
2023-07-21 15:44:47 -0400

    server

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS,', 'eCJbEumgT3SPBzlGEX1Rlw==','+nyEW8h3HXoL5OCkfu+Qwg==','mtAYsYQrorZXhkGU4JRudw==','pq/n53DFS40qe6qtnFZBNQ=='),
+    keys: env.array('APP_KEYS', 'eCJbEumgT3SPBzlGEX1Rlw==','+nyEW8h3HXoL5OCkfu+Qwg==','mtAYsYQrorZXhkGU4JRudw==','pq/n53DFS40qe6qtnFZBNQ=='),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
